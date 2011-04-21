865584a0c4c68cd71c54f507ea08f4d868ea3cc3
Henning Horn <post@henninghorn.dk>
2011-04-21 14:08:17 +0200

    HTML5, baby

--- index.php
+++ index.php
@@ -9,12 +9,9 @@ if($_SERVER['QUERY_STRING'] == 'reset') {
 }
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<meta chartset="utf-8"/>
 	<title>Daytrotter Explorer</title>
 	<link rel="stylesheet" href="inc/stylesheet.css" type="text/css" media="screen" charset="utf-8"/>
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>	
